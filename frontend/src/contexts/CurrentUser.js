890992498028719ea316dbacb41127cf348dbef6
TGlock <100971383+Aruin1@users.noreply.github.com>
2024-01-22 21:17:36 -0500

    first part done

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,12 +1,30 @@
 import { createContext, useState } from "react";
 
-
 export const CurrentUser = createContext()
+export default CurrentUserProvider
 
 function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
+   
+    
 
+
+    
+    
+    function CurrentUserProvider({ children }){
+    
+        const [currentUser, setCurrentUser] = useState(null)
+        window.setCurrentUser = setCurrentUser
+        return (
+            <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
+                {children}
+            </CurrentUser.Provider>
+        )
+    }
+    
+    
+    
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}
@@ -14,4 +32,3 @@ function CurrentUserProvider({ children }){
     )
 }
 
-export default CurrentUserProvider
